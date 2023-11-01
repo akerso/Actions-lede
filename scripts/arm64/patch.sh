d68bf811a391bb5ab3841fd3976f120f79708f9c
akerso <70606558+akerso@users.noreply.github.com>
2023-11-02 04:09:35 +0800

    Update patch.sh

--- scripts/arm64/patch.sh
+++ scripts/arm64/patch.sh
@@ -45,6 +45,6 @@ git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git
 #openclash
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 #msd_lite
-git clone -b main https://github.com/bauw2008/luci-app-msd.git
+#git clone -b main https://github.com/bauw2008/luci-app-msd.git
 
 popd
