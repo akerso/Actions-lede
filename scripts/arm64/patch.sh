a0c6748a6d061522a43ba2931de2fcb92372b8f6
akerso <70606558+akerso@users.noreply.github.com>
2023-11-06 03:41:37 +0800

    Update patch.sh

--- scripts/arm64/patch.sh
+++ scripts/arm64/patch.sh
@@ -46,7 +46,7 @@ git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 #msd_lite
 rm -rf feeds/packages/net/msd_lite
-git clone https://github.com/ximiTech/msd_lite.git feeds/packages/net/msd_lite
+git clone https://github.com/immortalwrt/packages/tree/openwrt-23.05/net/msd_lite.git feeds/packages/net/msd_lite
 #git clone -b main https://github.com/bauw2008/luci-app-msd.git
 
 popd
